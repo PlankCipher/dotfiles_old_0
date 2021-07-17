1b777c8552921b347bf123fe2712d9b59df8dcac
PlankCipher <plankcipher1@protonmail.com>
2021-07-17 02:15:28 +0200

    Toggle songs on clicking the music block in dwmblocks

--- .scripts/music.sh
+++ .scripts/music.sh
@@ -1,15 +1,27 @@
 #!/bin/sh
 
-CURRENT=$(mpc current | sed 's/\.mp3//')
-CURRENT_TRUNC=$(echo "$CURRENT" | cut -c -11 | sed 's/ $//')
+get_status () {
+  CURRENT=$(mpc current | sed 's/\.mp3//')
+  CURRENT_TRUNC=$(echo "$CURRENT" | cut -c -11 | sed 's/ $//')
 
-if [[ "$CURRENT_TRUNC" != "$CURRENT" ]]; then
-  CURRENT="$CURRENT_TRUNC..."
-fi
+  if [[ "$CURRENT_TRUNC" != "$CURRENT" ]]; then
+    CURRENT="$CURRENT_TRUNC..."
+  fi
 
-STATUS=$(mpc | awk 'NR==2 { print $1 }' | sed -E 's/\[|\]//g')
-if [[ "$STATUS" == "playing" ]]; then
-  echo -e "\x10\x0b $CURRENT"
-else
-  echo -e "\x10\x0b \x11$CURRENT\x0b"
-fi
+  STATUS=$(mpc | awk 'NR==2 { print $1 }' | sed -E 's/\[|\]//g')
+  if [[ "$STATUS" == "playing" ]]; then
+    echo -e "\x10\x0b $CURRENT"
+  else
+    echo -e "\x10\x0b \x11$CURRENT\x0b"
+  fi
+}
+
+case $1 in
+  1)
+    mpc toggle && sigdwmblocks 6
+    ;;
+
+  *)
+    get_status
+    ;;
+esac
