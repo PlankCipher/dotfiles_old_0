78856ad3e033758e05b8248b104bce1f0eaec811
PlankCipher <plankcipher1@protonmail.com>
2021-07-24 14:41:11 +0200

    Add zsh-syntax-highlighting plugin to ohmyzsh

--- spice_it_up.sh
+++ spice_it_up.sh
@@ -128,6 +128,9 @@ install_zsh_and_ohmyzsh () {
 
   # Install zsh-autosuggestions plugin
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+  # Install zsh-syntax-highlighting plugin
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 }
 
 install_dev_stuff () {
