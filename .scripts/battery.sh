78e076cc35fdef22777254022c3488b45e8ce6f1
PlankCipher <plankcipher1@protonmail.com>
2021-09-19 03:24:58 +0200

    Change icon based on status and notify on low battery in battery block

--- .scripts/battery.sh
+++ .scripts/battery.sh
@@ -1,16 +1,25 @@
 #!/bin/sh
 
-BATTERY=$(cat /sys/class/power_supply/BAT1/capacity)
+CAPACITY=$(cat /sys/class/power_supply/BAT0/capacity)
+STATUS=$(cat /sys/class/power_supply/BAT0/status)
 
 ICON=''
-if [[ $BATTERY -gt 74 ]]; then
-  ICON=''
-elif [[ $BATTERY -gt 49 ]]; then
-  ICON=''
-elif [[ $BATTERY -gt 24 ]]; then
-  ICON=''
+if [[ "$STATUS" == 'Charging' ]]; then
+  ICON=''
 else
-  ICON=''
+  if [[ $CAPACITY -gt 74 ]]; then
+    ICON=''
+  elif [[ $CAPACITY -gt 49 ]]; then
+    ICON=''
+  elif [[ $CAPACITY -gt 24 ]]; then
+    ICON=''
+  else
+    ICON=''
+  fi
+
+  if [[ $CAPACITY -le 15 ]]; then
+    notify-send -a 'Battery' "Battery is very low ($CAPACITY). Plug the charger."
+  fi
 fi
 
-echo -e "\x0d$ICON\x0b $BATTERY%"
+echo -e "\x0d$ICON\x0b $CAPACITY%"
