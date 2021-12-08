18b19386e58474605832a913cfc38235e2d55464
PlankCipher <plankcipher1@protonmail.com>
2021-12-08 20:02:23 +0200

    Add dell-g5se-fanctl package

--- spice_it_up.sh
+++ spice_it_up.sh
@@ -161,7 +161,7 @@ install_dev_stuff () {
 }
 
 install_other_dependencies () {
-  yay -S scrot feh vlc zathura zathura-pdf-mupdf ueberzug brave-bin python-pip python zip unzip bat discord freetube-bin franz-bin telegram-desktop libxft-bgra imagemagick i3lock fuse
+  yay -S scrot feh vlc zathura zathura-pdf-mupdf ueberzug brave-bin python-pip python zip unzip bat discord freetube-bin franz-bin telegram-desktop libxft-bgra imagemagick i3lock fuse dell-g5se-fanctl
 
   # Thunderbird
   sudo pacman -S thunderbird
