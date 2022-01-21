8bfb95d8779a5eb116133b6554241ebe25902c10
PlankCipher <plankcipher1@protonmail.com>
2022-01-21 23:10:20 +0200

    Add keybinding to jump to lua config files

--- .config/nvim/lua/plankcipher/keymappings/buffer_navigation.lua
+++ .config/nvim/lua/plankcipher/keymappings/buffer_navigation.lua
@@ -3,3 +3,31 @@ vim.api.nvim_set_keymap('n', '<leader>l', ':bnext<CR>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<leader>t', ':enew<CR>' , {})
 vim.api.nvim_set_keymap('n', '<leader>q', ':bp <BAR> bd #<CR>', {})
 vim.api.nvim_set_keymap('n', '<leader>s', '<C-W><C-W>', {noremap = true})
+
+function goToLuaConfigFile()
+  local HOME = os.getenv('HOME')
+
+  local current_line = vim.fn.getline('.')
+
+  local filename = ''
+  local terminating_quote = ''
+  for i = 1, #current_line do
+    local char = current_line:sub(i,i)
+    if terminating_quote == '' then
+      if char == '"' or char == "'" then
+        terminating_quote = char
+      end
+    else
+      if char == '"' or char == "'" then
+        break
+      else
+        filename = filename .. char
+      end
+    end
+  end
+
+  filename = HOME .. '/.config/nvim/lua/' .. filename:gsub('%.', '/') .. '.lua'
+  vim.cmd('edit ' .. filename)
+end
+
+vim.api.nvim_set_keymap('n', 'gl', '<cmd>lua goToLuaConfigFile()<CR>', {noremap = true})
