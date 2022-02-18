1b782d09ea686692c5f759ef29718684b2d04e35
PlankCipher <plankcipher1@protonmail.com>
2022-02-18 21:38:01 +0200

    neovim: add a keymapping for :History:

--- .config/nvim/lua/plankcipher/keymappings/fzf.lua
+++ .config/nvim/lua/plankcipher/keymappings/fzf.lua
@@ -1,3 +1,4 @@
 vim.api.nvim_set_keymap('n', '<leader>f', ':Files<CR>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<leader>b', ':Buffers<CR>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<leader>g', ':Rg<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>;', ':History:<CR>', {noremap = true})
