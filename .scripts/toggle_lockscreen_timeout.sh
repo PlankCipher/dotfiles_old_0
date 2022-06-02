428d42ea18b8cc53774cb73d5c34cacd0ee97b81
PlankCipher <plankcipher1@protonmail.com>
2022-06-02 21:24:33 +0200

    Add 5, 10, and 30 mins options to screenlock script

--- .scripts/toggle_lockscreen_timeout.sh
+++ .scripts/toggle_lockscreen_timeout.sh
@@ -3,12 +3,33 @@
 toggle_lockscreen_timeout () {
   pgrep xautolock > /dev/null
 
-  # If the previous command exited with non-zero
-  # status, then no xautolock process running
+  # If the previous command exited with a non-zero
+  # status, then no xautolock process is running
   if [[ $? -ne 0 ]]; then
     xautolock -time 1 -locker "$HOME/.scripts/i3lock_fancy_multimonitor/lock -n -p -g" &
   else
-    killall -9 xautolock > /dev/null
+    TIMEOUT=$(ps xo args | grep xautolock | grep -v grep | cut -d ' ' -f 3)
+
+    case $TIMEOUT in
+      1)
+        killall -9 xautolock > /dev/null
+        xautolock -time 5 -locker "$HOME/.scripts/i3lock_fancy_multimonitor/lock -n -p -g" &
+        ;;
+
+      5)
+        killall -9 xautolock > /dev/null
+        xautolock -time 10 -locker "$HOME/.scripts/i3lock_fancy_multimonitor/lock -n -p -g" &
+        ;;
+
+      10)
+        killall -9 xautolock > /dev/null
+        xautolock -time 30 -locker "$HOME/.scripts/i3lock_fancy_multimonitor/lock -n -p -g" &
+        ;;
+
+      30)
+        killall -9 xautolock > /dev/null
+        ;;
+    esac
   fi
 }
 
@@ -18,7 +39,7 @@ get_timeout () {
   if [[ $? -ne 0 ]]; then
     echo -e "\x0d\x0b Off"
   else
-    echo -e "\x0d\x0b On"
+    echo -e "\x0d\x0b $(ps xo args | grep xautolock | grep -v grep | cut -d ' ' -f 3)m"
   fi
 }
 
