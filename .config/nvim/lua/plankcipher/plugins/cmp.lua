a6056e7ba32eecc672299a0b70a357de088e44e0
PlankCipher <plankcipher1@protonmail.com>
2022-10-13 14:58:43 +0200

    feat(neovim): change nvim-cmp key mappings

--- .config/nvim/lua/plankcipher/plugins/cmp.lua
+++ .config/nvim/lua/plankcipher/plugins/cmp.lua
@@ -36,13 +36,13 @@ cmp.setup({
     end,
   },
   mapping = cmp.mapping.preset.insert({
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-k>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-j>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm {
+    ['<C-y>'] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
-    },
+    }),
     ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
