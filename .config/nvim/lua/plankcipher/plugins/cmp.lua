d16a658d1578988d7d7e10de40d11617ca0e6281
PlankCipher <plankcipher1@protonmail.com>
2022-10-29 00:33:47 +0200

    fix(nvim): don't cycle through nvim-cmp items with Tab & S-Tab

--- .config/nvim/lua/plankcipher/plugins/cmp.lua
+++ .config/nvim/lua/plankcipher/plugins/cmp.lua
@@ -44,18 +44,14 @@ cmp.setup({
       select = true,
     }),
     ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
+      if luasnip.expand_or_jumpable() then
         luasnip.expand_or_jump()
       else
         fallback()
       end
     end, { 'i', 's' }),
     ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
+      if luasnip.jumpable(-1) then
         luasnip.jump(-1)
       else
         fallback()
