057687edc05e1ea458039474ec273c5207fb6bd6
PlankCipher <plankcipher1@protonmail.com>
2023-01-02 15:51:21 +0200

    feat(nvim): revert back to the default commenting keybindings

--- .config/nvim/lua/plankcipher/plugins/comment.lua
+++ .config/nvim/lua/plankcipher/plugins/comment.lua
@@ -1,15 +1,1 @@
-require('Comment').setup({
-  toggler = {
-    line = 'g/c',
-    block = 'g/b',
-  },
-  opleader = {
-    line = 'g/',
-    block = 'gb',
-  },
-  extra = {
-    above = 'g/O',
-    below = 'g/o',
-    eol = 'g/A',
-  }
-})
+require('Comment').setup()
